f455091b194de3862c5fe1debf46f709f8660ffc
PaulSirois <pasirois@gmail.com>
2026-01-26 23:13:45 -0600

    Reimport of CF pack (mods only) all updated

--- cf2mr.sh
+++ cf2mr.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+pwd
 
-for file in /home/minecraft/.pw/degens_modded/*.pw.toml; do
+for file in ./resourcepacks/*.pw.toml; do
 	base=$(basename "$file")
 	slug="${base%%.*}"
 	packwiz rm $slug
 	packwiz mr add $slug
 done
+
